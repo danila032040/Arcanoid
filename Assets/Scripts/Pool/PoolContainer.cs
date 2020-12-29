b25f9b8866c443602b426ff437617c5aa964a06d
Даниил Радченко <37844948+danila032040@users.noreply.github.com>
2020-12-29 14:00:04 +0300

    Added PoolObjects commit

---
+++ Assets/Scripts/Pool/PoolContainer.cs
@@ -0,0 +1,70 @@
+﻿namespace Scripts.Pool
+{
+    using System;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    [Serializable]
+    public class PoolContainer
+    {
+        [SerializeField] private GameObject _prefabToPool;
+        [SerializeField] private Transform _container;
+
+        [SerializeField] private int _amountToPool;
+        [SerializeField] private bool _canExpand;
+
+        private List<GameObject> _pooledObjects;
+
+
+        public GameObject PrefabToPool => _prefabToPool;
+
+        public void Init()
+        {
+            _pooledObjects = new List<GameObject>();
+            for (int i = 0; i < _amountToPool; ++i)
+            {
+                AddNewObject();
+            }
+        }
+
+        public GameObject Get()
+        {
+            foreach(GameObject obj in _pooledObjects)
+            {
+                if (!obj.gameObject.activeSelf && !obj.gameObject.activeInHierarchy)
+                {
+                    obj.SetActive(true);
+                    obj.transform.SetParent(null);
+                    return obj;
+                }
+            }
+
+            if (_canExpand)
+            {
+                GameObject obj = AddNewObject();
+                obj.SetActive(true);
+                obj.transform.SetParent(null);
+                return obj;
+            }
+
+            return null;
+        }
+
+        public void Remove(GameObject obj)
+        {
+            if (_pooledObjects.Contains(obj))
+            {
+                obj.gameObject.SetActive(false);
+                obj.transform.SetParent(_container);
+            }
+        }
+
+        private GameObject AddNewObject()
+        {
+            GameObject obj = UnityEngine.Object.Instantiate(_prefabToPool);
+            _pooledObjects.Add(obj);
+            Remove(obj);
+            return obj;
+        }
+    }
+}
---
+++ Assets/Scripts/Pool/PoolManager.cs
@@ -0,0 +1,32 @@
+﻿namespace Scripts.Pool
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class PoolManager : MonoBehaviour
+    {
+        [SerializeField] private List<PoolContainer> _poolContainers = new List<PoolContainer>();
+
+        private readonly Dictionary<GameObject, PoolContainer> _poolPrefabsDictionary = new Dictionary<GameObject, PoolContainer>();
+
+        private void Start()
+        {
+            foreach(PoolContainer container in _poolContainers)
+            {
+                container.Init();
+                _poolPrefabsDictionary[container.PrefabToPool] = container;
+            }
+        }
+
+
+        public GameObject Get(GameObject orig)
+        {
+            return _poolPrefabsDictionary[orig].Get();
+        }
+
+        public void Remove(GameObject orig, GameObject obj)
+        {
+            _poolPrefabsDictionary[orig].Remove(obj);
+        }
+    }
+}
